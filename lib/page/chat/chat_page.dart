2db0dca2d1c511547e7f1d5d0eb211a6e81da4df
autumn-moon-py <1024120029@qq.com>
2023-11-17 20:07:56 +0800

    1117

--- lib/page/chat/chat_page.dart
+++ lib/page/chat/chat_page.dart
@@ -34,12 +34,12 @@ class _ChatPageState extends State<ChatPage> with WidgetsBindingObserver {
         // ignore: unused_local_variable
         final chatModel = context.read<ChatViewModel>();
         // chatModel.clearMessage();
-        // chatModel.changeLine(346);
+        // chatModel.changeLine(133);
         // chatModel.changeStartTime(0);
-        // chatModel.changeBeJump(368);
-        // chatModel.changeJump(328);
+        // chatModel.changeBeJump(104);
+        // chatModel.changeJump(0);
         // chatModel.changeResetLine(1529);
-        // chatModel.changeChap('第一章');
+        // chatModel.changeChap('第六章');
       }
       storyPlayer(context);
       Future.delayed(const Duration(milliseconds: 100), () {
--- lib/page/chat/chat_view_model.dart
+++ lib/page/chat/chat_view_model.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: use_build_context_synchronously
+
 import 'package:flutter/material.dart';
 import 'package:miko/page/chat/controller.dart';
 import 'package:miko/utils/chat_utils.dart';
